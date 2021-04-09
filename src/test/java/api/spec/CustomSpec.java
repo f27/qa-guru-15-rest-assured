88b0d4bbd1871a4062f63279d152b11d38ea4828
John Smoker <testgits@yandex.ru>
2021-04-10 02:42:53 +0500

    Added tests

--- src/test/java/api/spec/CustomSpec.java
+++ src/test/java/api/spec/CustomSpec.java
@@ -1,26 +1,16 @@
 package api.spec;
 
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 
 import static filter.LogFilter.filters;
-import static io.restassured.RestAssured.given;
 
 public class CustomSpec {
-    private final RequestSpecification request = given()
-            .baseUri("https://reqres.in")
-            .contentType(ContentType.JSON)
-            .filter(filters().withCustomTemplates())
-            .log().uri()
-            .when();
-
-
-
-    public static CustomSpec spec() {
-        return new CustomSpec();
-    }
-
-    public RequestSpecification request() {
-        return request;
-    }
+    public static RequestSpecification customSpec = new RequestSpecBuilder()
+            .setBaseUri("https://reqres.in")
+            .setContentType(ContentType.JSON)
+            .addFilter(new AllureRestAssured())
+            .build();
 }
--- src/test/java/api/steps/ApiSteps.java
+++ src/test/java/api/steps/ApiSteps.java
@@ -8,13 +8,14 @@ import io.qameta.allure.Step;
 
 import java.util.Map;
 
-import static api.spec.CustomSpec.spec;
+import static api.spec.CustomSpec.customSpec;
+import static io.restassured.RestAssured.given;
 
 public class ApiSteps {
 
     @Step("Получаем список пользователей со страницы {page}")
     public UsersData[] getUsers(int page) {
-        return spec().request()
+        return given(customSpec)
                 .get("/api/users?page=" + page)
                 .then()
                 .statusCode(200)
@@ -25,7 +26,7 @@ public class ApiSteps {
 
     @Step("Проверяем пользователя с id {id}")
     public UserData checkUser(String id) {
-        return spec().request()
+        return given(customSpec)
                 .get("/api/users/" + id)
                 .then()
                 .statusCode(200)
@@ -36,7 +37,7 @@ public class ApiSteps {
 
     @Step("Регистрация с данными {data}")
     public Register register(Map<String, String> data, int statusCode) {
-        return spec().request()
+        return given(customSpec)
                 .body(data)
                 .post("/api/register")
                 .then()
@@ -47,7 +48,7 @@ public class ApiSteps {
 
     @Step("Вход с данными {data}")
     public Login login(Map<String, String> data, int statusCode) {
-        return spec().request()
+        return given(customSpec)
                 .body(data)
                 .post("/api/login")
                 .then()
@@ -58,10 +59,9 @@ public class ApiSteps {
 
     @Step("Удаление пользователя с id {id}")
     public void deleteUser(int id) {
-        spec().request()
+        given(customSpec)
                 .delete("/api/users/" + id)
                 .then()
-                .statusCode(204)
-                .extract();
+                .statusCode(204);
     }
 }
